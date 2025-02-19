a9e277847e83173c6fecf94722983b698f8160bb
Andrey <suzdalev2005@gmail.com>
2025-02-19 15:51:15 +0300

    update endpoint

--- src/main/java/ru/andreyszdlv/demo/controller/MainController.java
+++ src/main/java/ru/andreyszdlv/demo/controller/MainController.java
@@ -1,13 +1,15 @@
 package ru.andreyszdlv.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-    @GetMapping("/hello-world")
-    public String helloWorld(){
-        return "Hello World!";
+    @GetMapping
+    public String get(@PathVariable String name) {
+        return "Hello, " + name + "!";
     }
+
 }
