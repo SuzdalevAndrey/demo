f52b431345cf7ef5bb8ba0056bd47f9caa7b5aae
Andrey <suzdalev2005@gmail.com>
2025-02-19 14:53:29 +0300

    init

---
+++ src/main/java/ru/andreyszdlv/demo/controller/MainController.java
@@ -0,0 +1,13 @@
+package ru.andreyszdlv.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}
